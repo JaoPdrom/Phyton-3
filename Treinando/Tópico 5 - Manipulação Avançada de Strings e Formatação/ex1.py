e726a9e793200042d17dd2839c2893c02ff208db
JaoPdrom <joaopedromissiagia@outlook.com>
2025-06-14 18:15:41 -0300

    Normalização de Nomes [Fácil]

---
+++ Treinando/Tópico 5 - Manipulação Avançada de Strings e Formatação/ex1.py
@@ -0,0 +1,16 @@
+'''
+Peça ao usuário para digitar seu nome completo. Exiba o nome com:
+
+1. Todas as letras em maiúsculas.
+2. Todas as letras em minúsculas.
+3. Apenas a primeira letra em maiúscula (capitalizada).
+
+O que praticar:
+Métodos .upper(), .lower() e .capitalize().
+'''
+
+nome = input('Digite seu nome completo: ')
+
+print('Nome em maiusculas:', nome.upper())
+print('Nome em minusculas:', nome.lower())
+print('Nome capitalizado:', nome.capitalize())
