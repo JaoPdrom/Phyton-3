766fe18d1660cb4907b6d7f467bae486e0e729d1
JaoPdrom <joaopedromissiagia@outlook.com>
2025-06-14 15:00:32 -0300

    Acessando Dados do Dicionário [Fácil]

---
+++ Treinando/Tópico 4 - Estruturas de Dados/ex2.py
@@ -0,0 +1,18 @@
+'''
+Crie um dicionário para armazenar informações sobre um livro: 
+titulo, autor e ano_publicacao. Preencha com dados fictícios e, 
+em seguida, exiba o autor do livro.
+
+O que praticar:
+Criação de dict, acesso a valores através de suas chaves.
+'''
+
+livro = {
+    'nome': 'a bela e a fera',
+    'autor': 'monteiro lobato',
+    'ano_publicacao': 2000,
+}
+
+print(livro['nome'])
+print(livro['autor'])
+print(livro['ano_publicacao'])
