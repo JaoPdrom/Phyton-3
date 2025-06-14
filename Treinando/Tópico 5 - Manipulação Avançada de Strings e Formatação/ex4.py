f4b35fbeff4017b08725ce6fadb7e8847ca951d6
JaoPdrom <joaopedromissiagia@outlook.com>
2025-06-14 18:35:04 -0300

    Inversão de String [Médio]

---
+++ Treinando/Tópico 5 - Manipulação Avançada de Strings e Formatação/ex4.py
@@ -0,0 +1,12 @@
+'''
+Peça ao usuário para digitar uma palavra. Use fatiamento 
+com passo negativo para inverter a palavra e exibi-la. 
+Por exemplo, "python" se tornaria "nohtyp".
+
+O que praticar:
+Fatiamento com passo ([::-1]).
+'''
+
+palavra = input('Digite uma palavra: ')
+
+print('Palavra invertida:', palavra[::-1])  
