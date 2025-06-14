5e50b4c73e748b7fab4b994d864caee86bf52f8b
JaoPdrom <joaopedromissiagia@outlook.com>
2025-06-14 18:12:35 -0300

    Estatísticas de Notas [Difícil] Mod

--- Treinando/Tópico 4 - Estruturas de Dados/ex10.py
+++ Treinando/Tópico 4 - Estruturas de Dados/ex10.py
@@ -3,20 +3,15 @@ Crie uma lista de dicionários, onde cada dicionário representa
 um aluno e contém as chaves nome e notas (uma lista de 3 notas). Exemplo:
 
 alunos = [
-
-{'nome': 'Alice', 'notas': [8, 9, 7]},
-
-{'nome': 'Bob', 'notas': [6, 7, 5]},
-
-{'nome': 'Carlos', 'notas': [10, 9, 9.5]}
-
+    {'nome': 'Alice', 'notas': [8, 9, 7]},
+    {'nome': 'Bob', 'notas': [6, 7, 5]},
+    {'nome': 'Carlos', 'notas': [10, 9, 9.5]}
 ]
 
 Escreva um programa que itere sobre essa lista e calcule a média de 
 cada aluno. Ao final, exiba o nome do aluno e sua respectiva média.
 
-**O que praticar:**
-
+O que praticar:
 Iteração sobre uma lista de dicionários, acesso a valores aninhados 
 (uma lista dentro de um dicionário), e cálculo com os dados obtidos.
 '''
@@ -28,5 +23,22 @@ alunos = [
 ]
 
 for aluno in alunos:
-    media = sum(aluno['notas']) / len(aluno['notas'])
+    media = 0
+    #armazena nota do aluno e as soma
+    for nota in aluno['notas']:
+        media += nota
+
+    #conta a quantidade de notas do aluno
+    contador = 0
+    for nota in aluno['notas']:
+        contador += 1
+
+    #calcula a media
+    media /= contador
     print(f'Media {aluno['nome']} eh {media:.2f}')
+
+
+#versao utilizando a funcao sum e len
+# for aluno in alunos:
+#     media = sum(aluno['notas']) / len(aluno['notas'])
+#     print(f'Media {aluno['nome']} eh {media:.2f}')
