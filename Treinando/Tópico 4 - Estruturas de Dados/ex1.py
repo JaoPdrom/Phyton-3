fbd85bea0a862a8e5b0c15938406461e6a43a45c
JaoPdrom <joaopedromissiagia@outlook.com>
2025-06-14 14:55:56 -0300

    Lista de Compras [Fácil]

---
+++ Treinando/Tópico 4 - Estruturas de Dados/ex1.py
@@ -0,0 +1,13 @@
+'''
+Crie uma lista com cinco itens de supermercado. 
+Em seguida, exiba o primeiro e o último item da lista.
+
+**O que praticar:**
+
+Criação de list, acesso a elementos por índice (positivo e negativo).
+'''
+
+lista = ['arroz', 'feijao', 'maca', 'pera']
+
+print(lista[1])
+print(lista[-1])
