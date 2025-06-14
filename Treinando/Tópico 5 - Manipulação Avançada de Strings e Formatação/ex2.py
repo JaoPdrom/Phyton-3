0c4aeb1608fd233e3a94ec8ad8ab50a24a84cfac
JaoPdrom <joaopedromissiagia@outlook.com>
2025-06-14 18:18:09 -0300

    Extraindo as Iniciais [Fácil]

---
+++ Treinando/Tópico 5 - Manipulação Avançada de Strings e Formatação/ex2.py
@@ -0,0 +1,11 @@
+'''
+Peça ao usuário para digitar uma palavra. Use fatiamento 
+(slicing) para exibir as três primeiras letras da palavra.
+
+O que praticar:
+Fatiamento de strings ([inicio:fim]).
+'''
+
+palavra = input('Digite uma palavra: ')
+
+print('As tres primeiras letras sao:', palavra[:3])
