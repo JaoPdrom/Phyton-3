54625dd472eb758ec0e41e1b4a648287703fd861
JaoPdrom <joaopedromissiagia@outlook.com>
2025-06-13 22:24:13 -0300

    Verificador de Maioridade [Fácil]

---
+++ Treinando/Tópico 2 - Operadores e Expressões/ex2.py
@@ -0,0 +1,13 @@
+'''
+Peça ao usuário para digitar sua idade. O programa deve exibir 
+True se a idade for maior ou igual a 18, e False caso contrário. 
+Não use if, apenas o resultado da comparação.
+
+O que praticar:
+
+Operador de comparação >=.
+'''
+
+idade = int(input('Informe a sua idade: '))
+
+print(idade >= 18)
