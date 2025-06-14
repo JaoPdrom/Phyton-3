4569683d42bda13b08b5a78caa371da9d600e848
JaoPdrom <joaopedromissiagia@outlook.com>
2025-06-13 23:31:21 -0300

    Contagem Regressiva com while [Fácil]

---
+++ Treinando/Tópico 3 - Controle de Fluxo/ex3.py
@@ -0,0 +1,14 @@
+'''
+Escreva um programa que faça uma contagem regressiva de 10 a 1. 
+Ao final, exiba a mensagem "Fogo!". Use um laço while.
+
+O que praticar:
+
+Laço while e operadores de atribuição (-=).
+'''
+
+contador = 10
+
+while contador >= 1:
+    print(contador)
+print('FOGO!')
