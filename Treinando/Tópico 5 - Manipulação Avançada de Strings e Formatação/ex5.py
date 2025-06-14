f2e18ae78e71d5503894da9ac3f55edbfbafda4d
JaoPdrom <joaopedromissiagia@outlook.com>
2025-06-14 18:37:49 -0300

    Palavras em uma Lista [Médio]

---
+++ Treinando/Tópico 5 - Manipulação Avançada de Strings e Formatação/ex5.py
@@ -0,0 +1,14 @@
+'''
+Dada a frase "Aprender Python é muito divertido", use o 
+método .split() para transformá-la em uma lista de palavras. 
+Exiba a lista resultante.
+
+O que praticar:
+Método .split().
+'''
+
+palavra = 'Aprender Python é muito divertido'
+
+lista_palavras = palavra.split()
+
+print('Lista de palavras:', lista_palavras)
