30cdcb760cf4c93992c6ef57e283e40ec7b84ed4
JaoPdrom <joaopedromissiagia@outlook.com>
2025-07-03 17:14:21 -0300

    Saudação Simples [Fácil]

---
+++ Treinando/Topico 6 Funcoes e Escopo/ex1.py
@@ -0,0 +1,11 @@
+'''
+Crie uma função chamada saudacao que não receba nenhum parâmetro 
+e simplesmente exiba a mensagem "Olá, programador!". Chame essa função para ver o resultado.
+
+O que praticar:
+Definição básica de uma função com def e chamada da função.
+'''
+def saudacao():
+    print("Olá, programador!")
+
+saudacao()
