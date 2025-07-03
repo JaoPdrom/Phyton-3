c77245d91d27aff9692a957275924214e7f756c2
JaoPdrom <joaopedromissiagia@outlook.com>
2025-07-03 18:13:49 -0300

    Função Lambda para Dobro [Fácil]

---
+++ Treinando/Topico 7 - Topicos Avancados e Pythonicos/ex3.py
@@ -0,0 +1,11 @@
+'''
+Crie uma função lambda que receba um número e retorne o seu dobro. 
+Armazene essa função em uma variável e use-a para calcular o dobro de um número.
+
+O que praticar:
+Sintaxe básica de funções lambda.
+'''
+
+dobro = lambda x: x * 2
+
+print(dobro(5))
