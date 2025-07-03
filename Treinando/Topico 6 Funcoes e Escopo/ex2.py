cbc9eb46f8e312673d7cde32bb8c8084b68e2b4e
JaoPdrom <joaopedromissiagia@outlook.com>
2025-07-03 17:15:54 -0300

    Saudação Personalizada [Fácil]

---
+++ Treinando/Topico 6 Funcoes e Escopo/ex2.py
@@ -0,0 +1,13 @@
+'''
+Modifique a função do exercício anterior para que ela 
+receba um parâmetro nome. A função deve exibir a mensagem 
+"Olá, [nome]!". Chame a função passando seu nome como argumento.
+
+O que praticar:
+Definição de funções com parâmetros e passagem de argumentos.
+'''
+
+def saudacao(nome):
+    print(f"Olá, {nome}!")
+
+saudacao("Joao")
