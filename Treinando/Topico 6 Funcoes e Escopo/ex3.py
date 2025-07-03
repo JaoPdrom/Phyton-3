77bf90c784b82af2718ecf0c153218b50a1043ed
JaoPdrom <joaopedromissiagia@outlook.com>
2025-07-03 17:16:42 -0300

    Função com Retorno [Fácil]

---
+++ Treinando/Topico 6 Funcoes e Escopo/ex3.py
@@ -0,0 +1,14 @@
+'''
+Crie uma função chamada soma que receba dois números como parâmetros 
+e retorne o resultado da soma. Chame a função, armazene o resultado 
+em uma variável e exiba essa variável.
+
+O que praticar:
+Uso da palavra-chave return para retornar um valor de uma função.
+'''
+
+def soma(num1, num2):
+    return num1 + num2
+
+resultado = soma(5, 3)
+print(f"O resultado da soma é: {resultado}")
