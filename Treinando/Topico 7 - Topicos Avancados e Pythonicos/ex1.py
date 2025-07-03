c3010673d10c030129da1a7e91db1e627527f35c
JaoPdrom <joaopedromissiagia@outlook.com>
2025-07-03 18:10:02 -0300

    Quadrados com List Comprehension [Fácil]

---
+++ Treinando/Topico 7 - Topicos Avancados e Pythonicos/ex1.py
@@ -0,0 +1,10 @@
+'''
+Crie uma lista com os quadrados dos números de 1 a 10 usando List Comprehension.
+
+O que praticar:
+Sintaxe básica de List Comprehension.
+'''
+
+quadrados = [x**2 for x in range(1, 11)]
+
+print(quadrados)
